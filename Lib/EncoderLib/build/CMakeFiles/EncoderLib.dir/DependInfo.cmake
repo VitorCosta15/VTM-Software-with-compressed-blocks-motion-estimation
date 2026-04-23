5930e323267b7e1da749cb78ca8f8c91eb9fa14d
VítorCosta15 <vscosta@inf.ufpel.edu.br>
2026-04-23 16:15:37 -0300

    update to git

---
+++ Lib/EncoderLib/build/CMakeFiles/EncoderLib.dir/DependInfo.cmake
@@ -0,0 +1,41 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/vitor/Desktop/vitech/VVCSoftware_VTM_23.6/source/Lib/EncoderLib/AQp.cpp" "CMakeFiles/EncoderLib.dir/AQp.obj" "gcc" "CMakeFiles/EncoderLib.dir/AQp.obj.d"
+  "C:/Users/vitor/Desktop/vitech/VVCSoftware_VTM_23.6/source/Lib/EncoderLib/BinEncoder.cpp" "CMakeFiles/EncoderLib.dir/BinEncoder.obj" "gcc" "CMakeFiles/EncoderLib.dir/BinEncoder.obj.d"
+  "C:/Users/vitor/Desktop/vitech/VVCSoftware_VTM_23.6/source/Lib/EncoderLib/CABACWriter.cpp" "CMakeFiles/EncoderLib.dir/CABACWriter.obj" "gcc" "CMakeFiles/EncoderLib.dir/CABACWriter.obj.d"
+  "C:/Users/vitor/Desktop/vitech/VVCSoftware_VTM_23.6/source/Lib/EncoderLib/EncAdaptiveLoopFilter.cpp" "CMakeFiles/EncoderLib.dir/EncAdaptiveLoopFilter.obj" "gcc" "CMakeFiles/EncoderLib.dir/EncAdaptiveLoopFilter.obj.d"
+  "C:/Users/vitor/Desktop/vitech/VVCSoftware_VTM_23.6/source/Lib/EncoderLib/EncCu.cpp" "CMakeFiles/EncoderLib.dir/EncCu.obj" "gcc" "CMakeFiles/EncoderLib.dir/EncCu.obj.d"
+  "C:/Users/vitor/Desktop/vitech/VVCSoftware_VTM_23.6/source/Lib/EncoderLib/EncGOP.cpp" "CMakeFiles/EncoderLib.dir/EncGOP.obj" "gcc" "CMakeFiles/EncoderLib.dir/EncGOP.obj.d"
+  "C:/Users/vitor/Desktop/vitech/VVCSoftware_VTM_23.6/source/Lib/EncoderLib/EncHRD.cpp" "CMakeFiles/EncoderLib.dir/EncHRD.obj" "gcc" "CMakeFiles/EncoderLib.dir/EncHRD.obj.d"
+  "C:/Users/vitor/Desktop/vitech/VVCSoftware_VTM_23.6/source/Lib/EncoderLib/EncLib.cpp" "CMakeFiles/EncoderLib.dir/EncLib.obj" "gcc" "CMakeFiles/EncoderLib.dir/EncLib.obj.d"
+  "C:/Users/vitor/Desktop/vitech/VVCSoftware_VTM_23.6/source/Lib/EncoderLib/EncLibCommon.cpp" "CMakeFiles/EncoderLib.dir/EncLibCommon.obj" "gcc" "CMakeFiles/EncoderLib.dir/EncLibCommon.obj.d"
+  "C:/Users/vitor/Desktop/vitech/VVCSoftware_VTM_23.6/source/Lib/EncoderLib/EncModeCtrl.cpp" "CMakeFiles/EncoderLib.dir/EncModeCtrl.obj" "gcc" "CMakeFiles/EncoderLib.dir/EncModeCtrl.obj.d"
+  "C:/Users/vitor/Desktop/vitech/VVCSoftware_VTM_23.6/source/Lib/EncoderLib/EncReshape.cpp" "CMakeFiles/EncoderLib.dir/EncReshape.obj" "gcc" "CMakeFiles/EncoderLib.dir/EncReshape.obj.d"
+  "C:/Users/vitor/Desktop/vitech/VVCSoftware_VTM_23.6/source/Lib/EncoderLib/EncSampleAdaptiveOffset.cpp" "CMakeFiles/EncoderLib.dir/EncSampleAdaptiveOffset.obj" "gcc" "CMakeFiles/EncoderLib.dir/EncSampleAdaptiveOffset.obj.d"
+  "C:/Users/vitor/Desktop/vitech/VVCSoftware_VTM_23.6/source/Lib/EncoderLib/EncSlice.cpp" "CMakeFiles/EncoderLib.dir/EncSlice.obj" "gcc" "CMakeFiles/EncoderLib.dir/EncSlice.obj.d"
+  "C:/Users/vitor/Desktop/vitech/VVCSoftware_VTM_23.6/source/Lib/EncoderLib/EncTemporalFilter.cpp" "CMakeFiles/EncoderLib.dir/EncTemporalFilter.obj" "gcc" "CMakeFiles/EncoderLib.dir/EncTemporalFilter.obj.d"
+  "C:/Users/vitor/Desktop/vitech/VVCSoftware_VTM_23.6/source/Lib/EncoderLib/InterSearch.cpp" "CMakeFiles/EncoderLib.dir/InterSearch.obj" "gcc" "CMakeFiles/EncoderLib.dir/InterSearch.obj.d"
+  "C:/Users/vitor/Desktop/vitech/VVCSoftware_VTM_23.6/source/Lib/EncoderLib/IntraSearch.cpp" "CMakeFiles/EncoderLib.dir/IntraSearch.obj" "gcc" "CMakeFiles/EncoderLib.dir/IntraSearch.obj.d"
+  "C:/Users/vitor/Desktop/vitech/VVCSoftware_VTM_23.6/source/Lib/EncoderLib/NALwrite.cpp" "CMakeFiles/EncoderLib.dir/NALwrite.obj" "gcc" "CMakeFiles/EncoderLib.dir/NALwrite.obj.d"
+  "C:/Users/vitor/Desktop/vitech/VVCSoftware_VTM_23.6/source/Lib/EncoderLib/RateCtrl.cpp" "CMakeFiles/EncoderLib.dir/RateCtrl.obj" "gcc" "CMakeFiles/EncoderLib.dir/RateCtrl.obj.d"
+  "C:/Users/vitor/Desktop/vitech/VVCSoftware_VTM_23.6/source/Lib/EncoderLib/SEIEncoder.cpp" "CMakeFiles/EncoderLib.dir/SEIEncoder.obj" "gcc" "CMakeFiles/EncoderLib.dir/SEIEncoder.obj.d"
+  "C:/Users/vitor/Desktop/vitech/VVCSoftware_VTM_23.6/source/Lib/EncoderLib/SEIFilmGrainAnalyzer.cpp" "CMakeFiles/EncoderLib.dir/SEIFilmGrainAnalyzer.obj" "gcc" "CMakeFiles/EncoderLib.dir/SEIFilmGrainAnalyzer.obj.d"
+  "C:/Users/vitor/Desktop/vitech/VVCSoftware_VTM_23.6/source/Lib/EncoderLib/SEIwrite.cpp" "CMakeFiles/EncoderLib.dir/SEIwrite.obj" "gcc" "CMakeFiles/EncoderLib.dir/SEIwrite.obj.d"
+  "C:/Users/vitor/Desktop/vitech/VVCSoftware_VTM_23.6/source/Lib/EncoderLib/VLCWriter.cpp" "CMakeFiles/EncoderLib.dir/VLCWriter.obj" "gcc" "CMakeFiles/EncoderLib.dir/VLCWriter.obj.d"
+  "C:/Users/vitor/Desktop/vitech/VVCSoftware_VTM_23.6/source/Lib/EncoderLib/WeightPredAnalysis.cpp" "CMakeFiles/EncoderLib.dir/WeightPredAnalysis.obj" "gcc" "CMakeFiles/EncoderLib.dir/WeightPredAnalysis.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ README.md
@@ -0,0 +1,22 @@
+# VTM-Software-with-compressed-blocks-motion-estimation
+
+# Author: Vítor Silva da Costa
+# Federal University of Pelotas
+
+This custom version of the VTM software was modified to apply the proposed Block Compression Algorithm to both the Search Area and the Coding Block during VVC motion estimation, which represents a fundamental stage of my undergraduate thesis.
+
+The modifications were implemented in the file Lib/EncoderLib/InterSearch.cpp, where each 4×4 Coding Block and 4×4 Candidate Block is compressed from 128 bits into 32-bit vectors.
+
+This approach was developed to reduce the memory usage and processing costs of dedicated hardware architectures for VVC motion estimation.
+
+This software simulation enables the evaluation of coding-efficiency losses and memory-access reductions resulting from the proposed approach compared to the baseline implementation of the VTM software.
+
+The full results of this research can be found in the following papers:
+
+https://doi.org/10.29292/jics.v20i1.964
+https://doi.org/10.1109/SBCCI66862.2025.11218658
+https://github.com/VitorCosta15/SAD-operating-unit-for-VVC-Motion-Estimation (the full undergraduate thesis is available here)
+
+The VHDL implementation of the proposed architecture can be found here:
+
+https://github.com/VitorCosta15/SAD-operating-unit-for-VVC-Motion-Estimation
